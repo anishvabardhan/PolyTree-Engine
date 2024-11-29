fb27bceda77bc0009fd0bfe8563988b44a99fd8f
Anishva Bardhan <54545359+anishvabardhan@users.noreply.github.com>
2024-11-29 14:22:33 -0600

    Delete Renderer directory

--- Renderer/CPUMesh.cpp
+++
@@ -1,8 +0,0 @@
-#include "CPUMesh.hpp"
-
-#include "Engine/Renderer/ObjLoader.hpp"
-
-bool CPUMesh::Load(std::string const& objFileName, Mat44& transform)
-{
-	return ObjLoader::Load(objFileName, transform, m_vertices, m_indices, m_hasNormals, m_hasUVs);
-}
